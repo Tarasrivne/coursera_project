ae7da7aa82baa1f82232ac2ad62f08d9d4f726ed
Tarasrivne <TarasViktorchuk@gmail.com>
2024-03-11 15:32:13 -0400

    Add item menu and templates

--- littlemon/restaurant/views.py
+++ littlemon/restaurant/views.py
@@ -3,7 +3,11 @@ from django.shortcuts import render
 # from .forms import BookingForm
 from .models import Menu
 
-
+# Add item menu
+def menu(request):
+    menu_data = Menu.objects.all()
+    main_data = {"menu": "menu_data"}
+    return render(request, 'menu.html', {"menu": "main_data"})
 
 # Create your views here.
 def home(request):
