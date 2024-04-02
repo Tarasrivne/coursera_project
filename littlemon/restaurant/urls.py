ccf4cbe1ee3ab12d2609510aeeb06ab42dbb51f3
Tarasrivne <TarasViktorchuk@gmail.com>
2024-04-02 15:40:29 -0400

    added some fix

--- littlemon/restaurant/urls.py
+++ littlemon/restaurant/urls.py
@@ -3,6 +3,7 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('', views.home, name="home"),
     path('about/', views.about, name="about"),
